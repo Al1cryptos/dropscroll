dfd974750cf17ec98b512db85efda59e90490be3
Al1crypto <107747989+Al1cryptos@users.noreply.github.com>
2024-11-22 12:30:43 +0100

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -2,7 +2,7 @@
 // File: @openzeppelin/contracts/token/ERC20/IERC20.sol
 
 
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
+// OpenZeppelin Contracts (token/ERC20/IERC20.sol)
 
 pragma solidity ^0.8.20;
 
